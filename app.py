eed9667c09c529b2a565d45d4f610d7aefb255ce
HansiHE <hansihe@hansihe.com>
2012-09-22 05:16:29 -0700

    added secret

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ application = Flask(__name__)
 application.config.from_object("local_config")
 application.config.from_object("config")
 
+SECRET_KEY="WeAOcOqFruPTsb6bXKNU"
+
 for blueprint in application.config["BLUEPRINTS"]:
     application.register_blueprint(**blueprint)
 
