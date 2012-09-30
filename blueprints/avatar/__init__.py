d9720db701682dcbf67ce0ff86ff78ef03cd147d
wiggitywhack <wiggitywhack@junction.at>
2012-09-30 13:18:13 -0700

    changed email for default avatar

--- blueprints/avatar/__init__.py
+++ blueprints/avatar/__init__.py
@@ -6,10 +6,11 @@ class Blueprint(flask.Blueprint):
     
     @staticmethod
     def avatar(mail):
+        default_email = "admin@junction.at"
         _mail = mail or ""
         hash = md5.new(_mail).hexdigest().lower()
         link = "http://www.gravatar.com/%s"%hash if _mail else None
-        default = urllib.quote("https://www.gravatar.com/avatar/%s.png?s="%md5.new("wiggitywhack@junction.at").hexdigest().lower())
+        default = urllib.quote("https://www.gravatar.com/avatar/%s.png?s="%md5.new(default_email).hexdigest().lower())
         img = ("https://www.gravatar.com/avatar/%s.png?r=pg&d="%hash)+default
         return type('Avatar', (object, ), dict(
                 link=link,
