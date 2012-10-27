1910e8b9ac2fea6004ae45285e8702f6f96fb122
HansiHE <hansihe@hansihe.com>
2012-10-27 05:15:41 -0700

    fixed up the bansystem structure a bit

--- blueprints/bans/systems/basesystem.py
+++ blueprints/bans/systems/basesystem.py
@@ -4,24 +4,47 @@ def getbans(user):
     Should return a dict containing info about the user.
     It should contain the following info:
       -   int : bancount
-      - O dict : bans
-          -   string : banreason
+      - O string : error - if this is returned, it is fine to not return anything else
+      - O array : bans
+          -   dict - info about the ban
+              - O string : issuer
+              - O string : reason
+              - O string : server	
           -   ...
     """
     return {"bancount" : 0}
 
+def getipbans(ip):
+    """
+    Should return a dict containing info about the ip.
+    If this is not supported by the system, just return.
+      -   int : bancount
+      - O string : error - if this is returned, it is fine to not return anything else
+      - O array : bans
+          -   dict - info about the ban
+              - O string : issuer
+              - O string : reason
+              - O string : server
+          -   ...
+    """
 
 def fulllookup(user):
     """
     Should return a dict containing info about the user.
     It should be containing the following info:
       -   int : bancount
-      - O dict : bans
-          -   string : banreason
+      - O string : error - if this is returned, it is fine to not return anything else
+      - O array : bans
+          -   dict - info about the ban
+              - O string : issuer
+              - O string : reason
+              - O string : server
           -   ...  
       - O int : playerrep - 0-10, the higher the better
-      - O dict : altlist
-          -   string : info - format is "username numbans"
+      - O array : altlist
+          -   dict - info about the alt
+              -   string : username
+              - O int : playerrep
           -   ...
     """
     return {"bancount" : 0}
