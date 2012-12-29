df3986d0008ee39e17126f4ada984e1b655376f7
HansiHE <hansihe@hansihe.com>
2012-12-28 20:47:52 -0700

    forgot base too :/

--- blueprints/base.py
+++ blueprints/base.py
@@ -1,6 +1,8 @@
 from flask import current_app
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cache import Cache
 
 db = SQLAlchemy(current_app)
 session = db.session
 Base = db.Model
+cache = Cache(current_app, config={'CACHE_TYPE': 'simple'})
