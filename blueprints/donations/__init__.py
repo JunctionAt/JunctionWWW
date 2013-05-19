72f7deac49f5d6251c0c2ba434edb15dd6ae79a6
Hans Elias J <hansihe@hansihe.com>
2013-05-19 17:41:43 -0400

    added a progress bar on the donations page

--- blueprints/donations/__init__.py
+++ blueprints/donations/__init__.py
@@ -3,6 +3,7 @@ __author__ = 'HansiHE'
 from flask import Blueprint, render_template, send_file
 from itsdangerous import URLSafeSerializer
 from blueprints.auth import current_user
+from donation_model import Transaction
 
 blueprint = Blueprint('donations', __name__,
                          template_folder='templates')
@@ -15,11 +16,11 @@ import ipn
 @blueprint.route('/donate')
 def donate():
     funds_current = 80
-    funds_target = 300
+    funds_target = 50
     return render_template(
         'donate_newer.html',
         funds_percentage=round((funds_current/float(funds_target))*100, 1),
         funds_target=funds_target,
-        funds_current=funds_current,
+        funds_current=Transaction.objects(valid=True).sum('amount'),
         signed_user=username_signer.dumps(current_user.name) if current_user.is_authenticated() else None
         )
