f0a1655a758213f0fcfc37ed0f927261aed2eba3
Hans Elias J <hansihe@hansihe.com>
2013-06-14 13:36:42 -0400

    pretty date format function

---
+++ util.py
@@ -0,0 +1,59 @@
+__author__ = 'HansiHE'
+
+import datetime
+
+
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.datetime.utcnow()
+    if type(time) is int:
+        diff = now - datetime.datetime.fromtimestamp(time)
+    elif isinstance(time, datetime.datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff / 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff / 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        num = day_diff/7
+        if num == 1:
+            return "a week ago"
+        else:
+            return str(num) + " weeks ago"
+    if day_diff < 365:
+        num = day_diff/30
+        if num == 1:
+            return "a month ago"
+        else:
+            return str(day_diff/30) + " months ago"
+
+    num = day_diff/365
+    if num == 1:
+        return "a year ago"
+    else:
+        return str(day_diff/365) + " years ago"
