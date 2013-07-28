e9f48bdf8497f70c87cb0e04d5ef0f07263ceb14
HansiHE <hansihe@hansihe.com>
2013-07-28 16:30:27 +0200

    fixed ordering of payments

--- blueprints/donations/views/payments_view.py
+++ blueprints/donations/views/payments_view.py
@@ -7,6 +7,6 @@ from flask import render_template
 
 @blueprint.route('/donate/payments')
 def payments_view():
-    payments = PaymentTransaction.objects().order_by('-period_begin')
+    payments = PaymentTransaction.objects().order_by('+period_begin')
 
     return render_template('payments_view.html', payments=payments)
