4e854b0b62723ff87a412e2a1bb07be711c69b9a
Tom Powell <zifnab@zifnab06.net>
2013-10-26 16:54:14 -0600

    Ban stuff - no changes

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -6,6 +6,8 @@ from blueprints.admin.view import ModelView
 from blueprints.base import admin
 from blueprints.forum.database.forum import Forum, Category, Board, Topic, Post
 from flask.ext.admin.model import typefmt
+from blueprints.bans.ban_model import Ban
+from blueprints.bans.appeal_model import AppealReply, Appeal
 import mongoengine
 
 from blueprints.auth.user_model import Role_Group, User
@@ -60,3 +62,19 @@ class Forum_Post_View(ModelView):
     pass
 
 admin.add_view(Forum_Post_View(Post, endpoint='admin_forum_posts', category='Mongo Forum'))
+
+#Bans and Appeals - UNSAFE - DO NOT USE
+#class Ban_View(ModelView):
+#	pass
+#
+#admin.add_view(Ban_View(Ban, endpoint='admin_bans_bans', category='Mongo Bans'))
+#
+#class Appeal_View(ModelView):
+#	pass
+#
+#admin.add_view(Appeal_View(Appeal, endpoint='admin_bans_appeal', category='Mongo Bans'))
+#
+#class Appeal_Reply_View(ModelView):
+#	pass
+#
+#admin.add_view(Appeal_Reply_View(AppealReply, endpoint='admin_bans_appeal_reply', category='Mongo Bans'))
