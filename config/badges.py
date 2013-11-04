b31c910a92b2b0de45b8ccacb9a0838627147f81
Hans Elias Josephsen <hansihe@hansihe.com>
2013-11-05 00:09:29 +0100

    file used tabs, shame on you

--- config/badges.py
+++ config/badges.py
@@ -6,25 +6,25 @@ BADGES = [
         'description': 'Actively serves on Junction staff',
         'priority': 2000
     },
-	{
-		'badge_id': 'staff',
-		'img_path': '/static/img/badges/award_fill.svg',
-		'name': 'Junction Staff',
-		'description': 'Actively serves on Junction staff',
+    {
+        'badge_id': 'staff',
+        'img_path': '/static/img/badges/award_fill.svg',
+        'name': 'Junction Staff',
+        'description': 'Actively serves on Junction staff',
         'priority': 1000
-	},
-	{
-		'badge_id': 'staff_emeritus',
-		'img_path': '/static/img/badges/heart_fill.svg',
-		'name': 'Staff Emeritus',
-		'description': 'Served on Junction staff once',
+    },
+    {
+        'badge_id': 'staff_emeritus',
+        'img_path': '/static/img/badges/heart_fill.svg',
+        'name': 'Staff Emeritus',
+        'description': 'Served on Junction staff once',
         'priority': 0
-	},
-	{
-		'badge_id': 'butts',
-		'img_path': '/static/img/badges/heart_stroke.svg',
-		'name': 'BUTTS',
-		'description': 'Butts are nice',
+    },
+    {
+        'badge_id': 'butts',
+        'img_path': '/static/img/badges/heart_stroke.svg',
+        'name': 'BUTTS',
+        'description': 'Butts are nice',
         'priority': 0
-	}
+    }
 ]
