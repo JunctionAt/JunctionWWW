4f56037872a09693afda1761a5d05bf017004e03
williammck <wjmck@me.com>
2013-11-17 02:36:21 -0600

    nah, randomize them!

--- blueprints/staff/views/staff_overview.py
+++ blueprints/staff/views/staff_overview.py
@@ -3,10 +3,13 @@ __author__ = 'HansiHE'
 from flask import render_template
 from .. import blueprint
 from blueprints.auth.user_model import User, Role_Group
+from random import shuffle
 
 @blueprint.route('/staff/')
 def view_staff():
-    mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").first()]).order_by('name')
-    techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").first()]).order_by('name')
+    mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").first()])
+    shuffle(mods)
+    techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").first()])
+    shuffle(techs)
 
     return render_template('staff_view_staff.html', mods=mods, techs=techs, title="Staff")
