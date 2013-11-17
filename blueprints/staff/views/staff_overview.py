5743b9ff703424dd5d8cbacd849ccd258397d357
williammck <wjmck@me.com>
2013-11-17 02:47:22 -0600

    randomize them for real this time!

--- blueprints/staff/views/staff_overview.py
+++ blueprints/staff/views/staff_overview.py
@@ -8,8 +8,10 @@ from random import shuffle
 @blueprint.route('/staff/')
 def view_staff():
     mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").first()])
+    mods = list(mods)
     shuffle(mods)
     techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").first()])
+    techs = list(techs)
     shuffle(techs)
 
     return render_template('staff_view_staff.html', mods=mods, techs=techs, title="Staff")
