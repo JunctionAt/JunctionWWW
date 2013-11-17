f3ac1a8e2b9333efa09698343db2b7d1948aa4c3
williammck <wjmck@me.com>
2013-11-17 02:22:46 -0600

    sort staff alphabetically

--- blueprints/staff/views/staff_overview.py
+++ blueprints/staff/views/staff_overview.py
@@ -6,7 +6,7 @@ from blueprints.auth.user_model import User, Role_Group
 
 @blueprint.route('/staff/')
 def view_staff():
-    mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").first()])
-    techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").first()])
+    mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").order_by('name').first()])
+    techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").order_by('name').first()])
 
     return render_template('staff_view_staff.html', mods=mods, techs=techs, title="Staff")
