3b3743ccac445274527421d1f0fee5abe3fec733
Hans Elias Josephsen <hansihe@hansihe.com>
2013-11-17 21:11:18 +0100

    staff listings are now defined in a list

--- blueprints/staff/views/staff_overview.py
+++ blueprints/staff/views/staff_overview.py
@@ -5,16 +5,25 @@ from .. import blueprint
 from blueprints.auth.user_model import User, Role_Group
 from random import shuffle
 
+listings = [
+    {"id": "moderator", "name": "Moderators", "description":
+        "These players complete modreqs, ban the troublemakers, and set up protections. "
+        "Just remember they have a life as well!"},
+    {"id": "technical", "name": "Technical Staff", "description":
+        "These players are trusted with the keys to the server. "
+        "They fixit when something crashes, code our custom plugins, and make sure everything's greased up."},
+    {"id": "inactive", "name": "Inactive Staff", "description":
+        "These players served Junction at some point in time. "
+        "They haven't been around in a while and therefore have had their powers removed."},
+]
+
 @blueprint.route('/staff/')
 def view_staff():
-    mods = User.objects(role_groups__in=[Role_Group.objects(name="moderator").first()])
-    mods = list(mods)
-    shuffle(mods)
-    techs = User.objects(role_groups__in=[Role_Group.objects(name="technical").first()])
-    techs = list(techs)
-    shuffle(techs)
-    inactives = User.objects(role_groups__in=[Role_Group.objects(name="inactive").first()])
-    inactives = list(inactives)
-    shuffle(inactives)
+    listings_data = dict()
+    for listing in listings:
+        data = User.objects(role_groups__in=[Role_Group.objects(name=listing["id"]).first()]).only('name')
+        data = list(data)
+        shuffle(data)
+        listings_data[listing["id"]] = data
 
-    return render_template('staff_view_staff.html', mods=mods, techs=techs, inactives=inactives, title="Staff")
+    return render_template('staff_view_staff.html', data=listings_data, listings=listings, title="Staff")
