518ab9d8e5b516ab96f2a247469c6a4151d37e28
Tom Powell <zifnab@zifnab06.net>
2013-11-18 16:51:03 -0700

    Added Mongo Alts in admin

--- blueprints/admin/__init__.py
+++ blueprints/admin/__init__.py
@@ -8,6 +8,7 @@ from blueprints.forum.database.forum import Forum, Category, Board, Topic, Post
 from flask.ext.admin.model import typefmt
 from blueprints.bans.ban_model import Ban
 from blueprints.bans.appeal_model import AppealReply, Appeal
+from blueprints.alts.alts_model import PlayerIpsModel, IpPlayersModel
 import mongoengine
 
 from blueprints.auth.user_model import Role_Group, User
@@ -81,3 +82,19 @@ admin.add_view(Ban_View(Ban, endpoint='admin_bans', category='Mongo Bans'))
 #	pass
 #
 #admin.add_view(Appeal_Reply_View(AppealReply, endpoint='admin_bans_appeal_reply', category='Mongo Bans'))
+
+
+#Alt Lookup
+class IpPlayer_View(ModelView):
+    can_delete = False
+    can_edit = False
+    can_create = False
+    column_searchable_list = ['ip']
+admin.add_view(IpPlayer_View(IpPlayersModel, endpoint='admin_alt_ip', category='Mongo Alts'))
+
+class PlayerIp_View(ModelView):
+    can_delete = False
+    can_edit = False
+    can_create = False
+    column_searchable_list = ['username']
+admin.add_view(PlayerIp_View(PlayerIpsModel, endpoint='admin_alt_username', category='Mongo Alts'))
