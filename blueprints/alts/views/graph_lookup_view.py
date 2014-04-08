b17d5833b6f1f195f4df9df0379a460784f7a53e
Hansihe <hansihe@hansihe.com>
2014-04-08 02:14:04 +0200

    revert alt graph permission

--- blueprints/alts/views/graph_lookup_view.py
+++ blueprints/alts/views/graph_lookup_view.py
@@ -10,7 +10,7 @@ from flask_wtf.csrf import generate_csrf
 @alts.route("/alts/graph/<string:player>")
 @login_required
 def graph_lookup_view(player=None):
-    if not current_user.has_permission('anathema.alts.graph'):
+    if not current_user.has_permission('alts.graph'):
         abort(403)
 
     return render_template("graph_view.html", preload=[player] if player else [], csrf_token=generate_csrf())
