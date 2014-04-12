06272cbe3a88b36039430fc85d40acb8f09700e6
Hansihe <hansihe@hansihe.com>
2014-04-12 13:12:52 +0200

    less code repetition for the proxies in base.py

--- blueprints/base.py
+++ blueprints/base.py
@@ -1,10 +1,15 @@
 from werkzeug.local import LocalProxy
 from . import extension_access
 
+
+def extension_access_proxy(name):
+    return LocalProxy(lambda: getattr(extension_access, name, None))
+
 # Mostly for backwards compatibility
-cache = LocalProxy(lambda: getattr(extension_access, "cache", None))
-mongo = LocalProxy(lambda: getattr(extension_access, "mongo", None))
-mail = LocalProxy(lambda: getattr(extension_access, "mail", None))
-admin = LocalProxy(lambda: getattr(extension_access, "admin", None))
-rest_api = LocalProxy(lambda: getattr(extension_access, "rest_api", None))
-markdown = LocalProxy(lambda: getattr(extension_access, "markdown", None))
+cache = extension_access_proxy("cache")
+mongo = extension_access_proxy("mongo")
+mail = extension_access_proxy("mail")
+admin = extension_access_proxy("admin")
+rest_api = extension_access_proxy("rest_api")
+markdown = extension_access_proxy("markdown")
+assets = extension_access_proxy("assets")
