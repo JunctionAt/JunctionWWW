388d727cfc25b779ff07955ece94837e7d1344a2
Hansihe <hansihe@hansihe.com>
2014-04-21 13:38:45 +0200

    improve error handling with uuids

--- models/player_model.py
+++ models/player_model.py
@@ -71,6 +71,8 @@ class MinecraftPlayer(Document):
 
         if mcname is None:
             mcname = uuid_utils.lookup_name(uuid)
+            if mcname is None:
+                raise LookupError("could not lookup mcname from uuid %s" % uuid)
         player.checkin_mcname(mcname)
 
         player.save()
