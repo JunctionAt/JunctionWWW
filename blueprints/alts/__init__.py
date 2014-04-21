cdc94a46c5a30f74ac7a95eed4523cb7e47368f2
Hansihe <hansihe@hansihe.com>
2014-04-21 10:12:17 +0200

    get mcname from player model in alts api

--- blueprints/alts/__init__.py
+++ blueprints/alts/__init__.py
@@ -49,7 +49,7 @@ class Alts(Resource):
         if user_ips:
             alt_objects = PlayerIpsModel.objects(ips__in=user_ips.ips, player__not=uuid)
             for alt_object in alt_objects:
-                alts.append({"alt": alt_object.username, "uuid": alt_object.player.uuid, "last_login": str(alt_object.last_login)})
+                alts.append({"alt": alt_object.player.mcname, "uuid": alt_object.player.uuid, "last_login": str(alt_object.last_login)})
         return {'alts': alts}
 
     post_parser = RequestParser()
