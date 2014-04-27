42dbe1e818b1a4e261cfe906c9c3c544f0b5a5f6
Hansihe <hansihe@hansihe.com>
2014-04-27 02:29:57 +0200

    apparently there is a new api, use it instead

--- blueprints/uuid_utils.py
+++ blueprints/uuid_utils.py
@@ -7,19 +7,16 @@ import requests
 import json
 from requests.exceptions import ConnectionError
 
-AGENT = "minecraft"
-PROFILE_URL = "https://api.mojang.com/profiles/page/{page}"
-UUID_PROFILE_URL = 'https://sessionserver.mojang.com/session/minecraft/profile/{uuid}'
-
-
 class NoSuchUserException(Exception): pass
 
+AGENT = "minecraft"
+PROFILE_URL = "https://api.mojang.com/profiles/minecraft"
+UUID_PROFILE_URL = 'https://sessionserver.mojang.com/session/minecraft/profile/{uuid}'
 
 class ProfileCriteria(dict):
     def __init__(self, name, agent):
         self['name'] = name
         self['agent'] = agent
-
 
 def get_profile(uuid, timeout=10):
     url = UUID_PROFILE_URL.format(uuid=uuid)
@@ -31,27 +28,25 @@ def get_profile(uuid, timeout=10):
 
     return profile
 
-
 def get_uuid(*name, **kwargs):
     timeout = 10
     if "timeout" in kwargs:
         timeout = kwargs["timeout"]
     if len(name) == 0:
         return None
-    crit = [ProfileCriteria(x, AGENT) for x in name]
     p = []
 
     page = 1
     while True:
-        url = PROFILE_URL.format(page=page)
+        if len(name) == 0:
+            break
+        crit = name[:100]
+        name = name [100:]
         data = json.dumps(crit)
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        r = requests.post(url, data=data, headers=headers, timeout=timeout)
+        r = requests.post(PROFILE_URL, data=data, headers=headers, timeout=timeout)
         profiles = r.json()
-        if 'profiles' in profiles:
-            if profiles['size'] == 0:
-                break
-            p.extend(profiles['profiles'])
+        p.extend(profiles)
 
         page += 1
                 
