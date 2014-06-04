465976d7e82324cbedfd169ff3ebdd32eb86b582
Hansihe <hansihe@hansihe.com>
2014-06-04 21:17:33 +0200

    decrement messages count when they are marked as read

--- assets/js/junction/notifications.js
+++ assets/js/junction/notifications.js
@@ -13,6 +13,10 @@ Molecular.register("notifications_tools", function() {
 
             mark("read", id, function() {
                 row.addClass("read");
+
+                // Decrement the notification count in the header.
+                var header_notification_num_display = $('.header-notifcation-num-display');
+                header_notification_num_display.html(parseInt(header_notification_num_display.html(), 0) - 1)
             })
         }
    })
